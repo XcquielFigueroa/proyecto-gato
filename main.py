f5d9732b82ef25843372085c20bf6d8bdbd0c729
XcquielFigueroa <exequiel.figueroa@virginiogomez.cl>
2025-06-08 08:25:32 -0400

    Agrega contador de victorias

--- main.py
+++ main.py
@@ -35,18 +35,17 @@ def movimiento_ia(tablero):
 
 #revisar ganador
 def hay_ganador(tablero):
-    #verificar filas y columnas
     for i in range(3):
         if tablero[i][0] == tablero[i][1] == tablero[i][2] != " ":
             return True
         if tablero[0][i] == tablero[1][i] == tablero[2][i] != " ":
             return True
-    #verificar diagonales
     if tablero[0][0] == tablero[1][1] == tablero[2][2] != " ":
         return True
     if tablero[0][2] == tablero[1][1] == tablero[2][0] != " ":
         return True
     return False
+
 
 #verificar tablero lleno
 def tablero_lleno(tablero):
@@ -55,27 +54,55 @@ def tablero_lleno(tablero):
             return False
     return True
 
+#finalizador de partida segun puntaje
+def finalizar_partida(ganador_partida, x, o):
+    diferencia = abs(x - o)
+    if x == 10 or o == 10:
+        print(f"Resultados:\nJugador X = {x}\nJugador 0 = {o}")
+        print(f"\033[32mTEl jugador {ganador_partida} ha ganado la partida con {diferencia} puntos de ventaja\033[0m")
+        return False
+    else:
+        return True
+
 #funcion principal
-def juego_completo():
+def juego():
     tablero = crear_tablero()
     jugador_actual = "X"
-    while True:
-        imprimir_tablero(tablero)
-        print(f"Turno de {jugador_actual}")
-        if jugador_actual == "X":
-            movimiento_jugador(tablero, jugador_actual)
-        else:
-            movimiento_ia(tablero)
-        if hay_ganador(tablero):
+    puntos_x = 0
+    puntos_o = 0
+    while finalizar_partida(jugador_actual, puntos_x, puntos_o):
+        while True:
+            print(f"jugador X = {puntos_x} puntos\njugador O = {puntos_o} puntos\n")
             imprimir_tablero(tablero)
-            print(f"¡{jugador_actual} ha ganado!")
-            break
-        if tablero_lleno(tablero):
-            print("¡Empate!")
-        if jugador_actual == "O":
-            jugador_actual = "X"
-        else:
-            jugador_actual = "O"
+            print(f"\033[33mTurno de {jugador_actual}\033[0m\n")
 
-#ejecución el juego
-juego_completo()
+            if jugador_actual == "X":
+                movimiento_jugador(tablero, jugador_actual)
+
+            else:
+                movimiento_ia(tablero)
+            if hay_ganador(tablero):
+                imprimir_tablero(tablero)
+                print(f"¡{jugador_actual} ha ganado!")
+                if jugador_actual == "X":
+                    puntos_x += 1
+                else:
+                    puntos_o += 1
+                tablero = crear_tablero()
+                break
+
+            if tablero_lleno(tablero):
+                print("¡Empate!")
+                tablero = crear_tablero()
+
+            if jugador_actual == "O":
+                jugador_actual = "X"
+
+            else:
+                jugador_actual = "O"
+
+#evitar error al cerrar
+try:
+    juego()
+except KeyboardInterrupt:
+    print("\nJuego cerrado por el usuario")
